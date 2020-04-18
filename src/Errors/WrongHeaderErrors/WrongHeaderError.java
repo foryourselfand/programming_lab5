cd5c2e5ab2454cb6935d68500870171f91faef58
foryourselfand <foryourselfand@gmail.com>
2020-04-19 00:57:19 +0300

    scripts and collections

--- src/Errors/WrongHeaderErrors/WrongHeaderError.java
+++ src/Errors/WrongHeaderErrors/WrongHeaderError.java
@@ -7,6 +7,6 @@ public class WrongHeaderError extends InputErrorFull {
 	public static String MESSAGE_EXTRA = "Лишние элементы: ";
 	
 	public WrongHeaderError(String message) {
-		super("Неверный заголовок csv файла: \n" + message + "\nШаблон: ");
+		super("Неверный заголовок csv файла: \n" + message + "\n");
 	}
 }
